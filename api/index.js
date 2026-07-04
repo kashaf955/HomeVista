d1fedd784014df1dd232d3f0d47989e074844abf
kashaf955 <kashaftariq496@gmail.com>
2026-07-04 12:55:21 +0500

    connect to DataBase

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 const app = express();  
 app.listen(3000, () => {

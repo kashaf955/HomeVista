2e3e15af04af2a4ba276bb56eaf976c7cec373c3
kashaf955 <kashaftariq496@gmail.com>
2026-07-08 16:40:29 +0500

    Complete Google OAuth

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
+  },
+  avatar: {
+    type: String,
+    default: "https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8="
   }
 } ,{ timestamps: true });
 
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
     const handleGoogleClick = async () => {
         try {
@@ -11,15 +16,27 @@ export default function OAuth() {
 
 
             const result = await signInWithPopup(auth, provider);
-            console.log(result);
-            
+            const res = await fetch('/api/auth/google', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: result.user.email,
+                    username: result.user.displayName,
+                    photo: result.user.photoURL,
+                }),
+            });
+            const data = await res.json();
+            dispatch(signInSuccess(data));
+            navigate('/');
         } catch (error) {
             console.error('Could not signin with Google', error);
         }
     };
 
   return (
-    <button onClick={handleGoogleClick} type='button' className='bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 uppercase items-center w-full mt-4'>
+    <button onClick={handleGoogleClick} type='button' className='bg-black text-white py-2 px-4 rounded-md hover:bg-blue-600 uppercase items-center w-full mt-4'>
       Continue with Google
     </button>
   )

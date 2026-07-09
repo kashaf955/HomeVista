ee51ca136789ca8c0675071f0fcd1c0edece3472
kashaf955 <kashaftariq496@gmail.com>
2026-07-09 18:17:46 +0500

    Update depreciated method

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -16,14 +16,18 @@ export const updateUser = async(req, res,next) => {
     {
       req.body.password = bcryptjs.hashsync(req.body.password, 10)
     }
-    const updateUser = await User.findByIdAndUpdate( req.params.id,{
-      $set:{
-        username: req.body.username,
-        email: req.body.email,
-        password : req.body.password,
-        avatar: req.body.avatar,
-      }
-    }, {new: true})
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { returnDocument: "after" }
+    )
 
     const {password, ...otherDetails} = updateUser._doc;
   } catch (error) {

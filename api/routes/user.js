ee9d7c3d4acdda407648f5d3debe6626be139e3f
kashaf955 <kashaftariq496@gmail.com>
2026-07-09 16:32:51 +0500

    complete Image Upload Functionality

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,5 @@
 import express from "express";
-import mongoose from "mongoose";
 import { test } from "../controllers/user.controller.js";
-
-
-
 
 const router = express.Router();
 
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,17 +1,63 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase'
 
 export default function Profile() {
-  const {currentUser} = useSelector((state) => state.user)
+  const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const fileRef = useRef(null);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError ] = useState(false);
+  const [formData, setFormData] = useState({});
+  
+  useEffect(() => {
+    if (!file) return
+
+    const storage = getStorage(app)
+    const fileName = `${Date.now()}-${file.name}`
+    const storageRef = ref(storage, fileName)
+    const uploadTask = uploadBytesResumable(storageRef, file)
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData((prev) => ({ ...prev, avatar: downloadURL }))
+        })
+      }
+    )
+  }, [file])
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-bold text-center my-6'>Profile</h1>
-      <form action="" className='flex flex-col gap-3'>
-        <img src= {currentUser.avatar} alt="Profile" srcset="" className='h-20 w-20 rounded-full object-cover cursor-pointer self-center mt-2' />
-         <input type="text" placeholder='Username' className='border border-red-800 rounded-md p-2 w-full mb-4' id='username' />
-        <input type="email" placeholder='Email' className='border border-red-800 rounded-md p-2 w-full mb-4' id='email'  />
-        <input type="password" placeholder='Password' className='border border-red-800  rounded-md p-2 w-full mb-4' id='password' />
-        <button type="submit" className='bg-red-800 text-white py-2 px-4 rounded-md hover:bg-red-600 uppercase w-full'>
+      <form action='' className='flex flex-col gap-3'>
+        <input onChange={(e) => setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*'/>
+        <img src={formData.avatar || currentUser.avatar} onClick={() => fileRef.current?.click()} alt='Profile' srcSet='' className='h-20 w-20 rounded-full object-cover cursor-pointer self-center mt-2' />
+        <p className='flex text-center'>
+          {fileUploadError ? (
+            <span className='text-red-800 text-center mx-auto'>Error Uploading Image</span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-black text-center mx-auto'>Uploading {filePerc}%</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-600 text-center mx-auto'>Image Uploaded Successfully</span>
+          ) : (
+            ''
+          )}
+        </p>
+        {/* <button type='button' onClick={() => fileRef.current?.click()} className='bg-red-800 text-white py-2 px-4 rounded-md hover:bg-red-600 uppercase w-1/2 mx-auto m-4'>Add Image</button> */}
+         <input type='text' placeholder='Username' className='border border-red-800 rounded-md p-2 w-full mb-4' id='username' />
+        <input type='email' placeholder='Email' className='border border-red-800 rounded-md p-2 w-full mb-4' id='email'  />
+        <input type='password' placeholder='Password' className='border border-red-800  rounded-md p-2 w-full mb-4' id='password' />
+        <button type='submit' className='bg-red-800 text-white py-2 px-4 rounded-md hover:bg-red-600 uppercase w-full'>
           Update
         </button>
       </form>

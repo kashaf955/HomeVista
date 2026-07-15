14dd6f3eed66a69a8b0cdce012bd9a75f6c87737
kashaf955 <kashaftariq496@gmail.com>
2026-07-15 22:47:52 +0500

    Final Chnages for pre installation

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/listing", listingRouter);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
-app.get("*", (req, res) => {
+app.get("/{*path}", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
